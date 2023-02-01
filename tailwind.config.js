ad1696275bb0c209393601fb2a63fb8b22715a7d
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-01 01:08:01 -0600

    Set tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{vue, js, ts, jsx, tsx}'
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
