639daeeedbf6d6ae34edb3f9b9007942649587ab
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-01 01:08:42 -0600

    Set color scheme

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+html.dark {
+  color-scheme: dark;
+}
+
+body {
+  @apply bg-green-400 text-gray-100 dark:bg-slate-800 dark:text-gray-200 text-lg;
+  transition: background-color 0.2s ease-out, color 0.2s ease-out;
+}
+</style>
