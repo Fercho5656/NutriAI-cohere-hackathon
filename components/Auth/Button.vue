3a843444e7e8b2f680b167caac954137e2f852ed
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-03 23:56:40 -0600

    Add auth button component

---
+++ components/Auth/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="flex items-center gap-x-3 p-4 rounded-lg 
+    bg-slate-800 hover:bg-slate-600 text-white font-bold font-sans text-lg 
+    dark:bg-green-600 dark:hover:bg-green-700" @click="emits('login', props.provider)">Sign up with
+    <icon class="text-white" />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  provider: 'github' | 'google'
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits(['login'])
+const icon = defineAsyncComponent(() => import(`~/components/Icons/${props.provider}.vue`))
+</script> 
+
+<style scoped>
+
+</style>
