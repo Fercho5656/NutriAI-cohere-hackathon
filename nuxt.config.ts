340c5c69c9058e6caa6a1a48631fa418aeb24e0c
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-03 23:56:06 -0600

    Install supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,3 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
-  image: {
-    presets: {
-      cover: {
-        modifiers: {
-          fit: 'cover',
-          format: 'webp',
-        }
-      }
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge', '@nuxtjs/supabase'],
 })
