ef031b98d11a8346a1e676f3e60d11bcda4da62a
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-06 21:42:32 -0600

    Add server endpoint to fetch eating plans

---
+++ server/api/plan.get.ts
@@ -0,0 +1,21 @@
+import { serverSupabaseClient } from "#supabase/server"
+import IPlan from "~~/interfaces/IPlan"
+import ISupabaseQuery from "~~/interfaces/ISupabaseQuery"
+
+export default defineEventHandler(async (event): Promise<ISupabaseQuery<IPlan[]>> => {
+  const client = serverSupabaseClient(event)
+  const { data, error } = await client
+    .from('plan')
+    .select('*')
+  if (data == null) return { data: [], error }
+  return { data, error }
+})
+
+/* export const getAllPlans = async (): Promise<ISupabaseQuery<IPlan[]>> => {
+  const client = useSupabaseClient()
+  const { data, error } = await client
+    .from('plan')
+    .select('*')
+  if (data == null) return { data: [], error }
+  return { data, error }
+} */
