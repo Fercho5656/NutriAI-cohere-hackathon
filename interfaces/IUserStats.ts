f0b8123bc11d77e43bc5d2d83a192a60cd00c437
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-07 01:14:30 -0600

    Add interface for user stats

---
+++ interfaces/IUserStats.ts
@@ -0,0 +1,12 @@
+export interface IUserStats {
+  id?: number
+  user_id?: string
+  weight: string
+  height: string
+  age: string
+  activity_factor: string
+  gender: string
+  imc: string
+  energy_requirement: string
+  created_at?: Date
+}
