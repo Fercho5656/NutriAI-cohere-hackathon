f6f2484b43bdb157f630a136dea86f01d266a518
Fernando Moreno <morenomoranfernando@gmail.com>
2023-02-07 12:47:24 -0600

    aa

--- server/api/cohere.post.ts
+++ server/api/cohere.post.ts
@@ -8,6 +8,19 @@ export default defineEventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig()
   console.log(`COHERE_KEY: ${runtimeConfig.COHERE_KEY}`)
 
+  const data = {
+    model: 'xlarge',
+    prompt: `Write an eating plan of 1780 kilocalories in JSON format:\n{\n\"name\": \"Nourish and Fuel\",\n\"kilocalories\": 1780,\n\"breakfast\": [\"1 cup of oatmeal with 1/2 cup of berries and 1 tsp of honey\", \"1 cup of Greek yogurt with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"lunch\": [\"2 cups of mixed greens with 1 cup of grilled chicken, 1/4 cup of crumbled feta cheese, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\"],\n\"snack\": [\"1 cup of unsweetened almond milk with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"dinner\": [\"1 cup of brown rice with 1 cup of grilled chicken, 1/4 cup of steamed broccoli, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\", \"1 apple\"]\n}\n--\nWrite an eating plan of ${energyRequirement - 500} kilocalories in JSON format:`,
+    max_tokens: 40,
+    temperature: 0.3,
+    k: 0,
+    p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop_sequences: ['--'],
+    return_likelihoods: 'NONE',
+  }
+
   const { results } = await $fetch(COHERE_API_GENERATE_URL, {
     method: 'POST',
     headers: {
@@ -16,9 +29,7 @@ export default defineEventHandler(async (event) => {
       "Content-Type": 'application/json',
       "Cohere-Version": '2022-12-06'
     },
-    body: {
-      texts: [`Write an eating plan of 1780 kilocalories in JSON format:\n{\n\"name\": \"Nourish and Fuel\",\n\"kilocalories\": 1780,\n\"breakfast\": [\"1 cup of oatmeal with 1/2 cup of berries and 1 tsp of honey\", \"1 cup of Greek yogurt with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"lunch\": [\"2 cups of mixed greens with 1 cup of grilled chicken, 1/4 cup of crumbled feta cheese, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\"],\n\"snack\": [\"1 cup of unsweetened almond milk with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"dinner\": [\"1 cup of brown rice with 1 cup of grilled chicken, 1/4 cup of steamed broccoli, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\", \"1 apple\"]\n}\n--\nWrite an eating plan of ${energyRequirement - 500} kilocalories in JSON format:`]
-    }
+    body: JSON.stringify(data)
   }).then(res => res.json())
   return results
 
