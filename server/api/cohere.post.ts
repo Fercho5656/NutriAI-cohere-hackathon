9718b8de8f46389a42311976bf26c61f840f3b90
Fernando Moreno <morenomoranfernando@gmail.com>
2023-02-07 09:01:39 -0600

    Find JSON in raw string

--- server/api/cohere.post.ts
+++ server/api/cohere.post.ts
@@ -1,4 +1,3 @@
-import { serverSupabaseClient, serverSupabaseUser } from "#supabase/server"
 import cohere from 'cohere-ai'
 
 export default defineEventHandler(async (event) => {
@@ -8,7 +7,7 @@ export default defineEventHandler(async (event) => {
   cohere.init(runtimeConfig.COHERE_KEY)
   const response = await cohere.generate({
     model: 'xlarge',
-    prompt: `Write an eating plan of ${energyRequirement} kilocalories in JSON format:\n{\n\"name\": \"Nourish and Fuel\",\n\"kilocalories\": 1780,\n\"breakfast\": [\"1 cup of oatmeal with 1/2 cup of berries and 1 tsp of honey\", \"1 cup of Greek yogurt with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"lunch\": [\"2 cups of mixed greens with 1 cup of grilled chicken, 1/4 cup of crumbled feta cheese, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\"],\n\"snack\": [\"1 cup of unsweetened almond milk with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"dinner\": [\"1 cup of brown rice with 1 cup of grilled chicken, 1/4 cup of steamed broccoli, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\", \"1 apple\"]\n}\n--\nWrite an eating plan of 1930 kilocalories in JSON format:`,
+    prompt: `Write an eating plan of 1780 kilocalories in JSON format:\n{\n\"name\": \"Nourish and Fuel\",\n\"kilocalories\": 1780,\n\"breakfast\": [\"1 cup of oatmeal with 1/2 cup of berries and 1 tsp of honey\", \"1 cup of Greek yogurt with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"lunch\": [\"2 cups of mixed greens with 1 cup of grilled chicken, 1/4 cup of crumbled feta cheese, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\"],\n\"snack\": [\"1 cup of unsweetened almond milk with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"dinner\": [\"1 cup of brown rice with 1 cup of grilled chicken, 1/4 cup of steamed broccoli, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\", \"1 apple\"]\n}\n--\nWrite an eating plan of ${energyRequirement} kilocalories in JSON format:`,
     max_tokens: 350,
     temperature: 2,
     k: 0,
@@ -18,5 +17,10 @@ export default defineEventHandler(async (event) => {
     stop_sequences: [],
     return_likelihoods: 'NONE'
   })
-  console.log(`Prediction: ${response.body.generations[0].text}`);
+  const str = response.body.generations[0].text
+  console.log(str)
+  const splitStr = str.split('--')[0]
+  const strIdx = splitStr.indexOf('}')
+  const strObj = splitStr.slice(0, strIdx + 1)
+  return JSON.parse(strObj)
 })
