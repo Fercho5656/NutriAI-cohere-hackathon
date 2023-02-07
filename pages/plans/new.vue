f9af72a1eb5b1344d89b9696509e826025631075
Fernando Moreno <morenomoranfernando@gmail.com>
2023-02-07 13:03:59 -0600

    weqwe

--- pages/plans/new.vue
+++ pages/plans/new.vue
@@ -110,14 +110,14 @@ const onSubmit = async () => {
   const dataa = {
     model: 'xlarge',
     prompt: `Write an eating plan of 1780 kilocalories in JSON format:\n{\n\"name\": \"Nourish and Fuel\",\n\"kilocalories\": 1780,\n\"breakfast\": [\"1 cup of oatmeal with 1/2 cup of berries and 1 tsp of honey\", \"1 cup of Greek yogurt with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"lunch\": [\"2 cups of mixed greens with 1 cup of grilled chicken, 1/4 cup of crumbled feta cheese, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\"],\n\"snack\": [\"1 cup of unsweetened almond milk with 1/2 cup of mixed nuts and 1 tsp of honey\"],\n\"dinner\": [\"1 cup of brown rice with 1 cup of grilled chicken, 1/4 cup of steamed broccoli, and 1/4 cup of sliced red onion\", \"1/2 cup of cooked quinoa with 1/4 cup of black beans, 1/4 cup of chopped tomato, and 1/4 cup of chopped avocado\", \"1 cup of unsweetened almond milk\", \"1 apple\"]\n}\n--\nWrite an eating plan of ${1500 - 500} kilocalories in JSON format:`,
-    max_tokens: 40,
-    temperature: 0.3,
+    max_tokens: 350,
+    temperature: 2,
     k: 0,
-    p: 1,
+    p: 0.75,
     frequency_penalty: 0,
     presence_penalty: 0,
-    stop_sequences: ['--'],
-    return_likelihoods: 'NONE',
+    stop_sequences: [],
+    return_likelihoods: 'NONE'
   }
 
   const response = fetch('https://api.cohere.ai/generate', {
