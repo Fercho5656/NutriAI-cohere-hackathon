9302823ffceca94212271972d9edaab2a069cf04
Fernando Moreno <morenomoranfernando@gmail.com>
2023-02-07 12:06:59 -0600

    Add cohere key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge', '@nuxtjs/supabase'],
   runtimeConfig: {
-    COHERE_KEY: ''
+    COHERE_KEY: '4vQrZ6kHFoNh3Ufv2qxMrFSAhgGFuKL7NmsgOpd2'
   }
 })
