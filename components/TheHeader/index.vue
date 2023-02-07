487e26e922e106875fc1db910b45e42d96bcd91f
Fercho5656 <morenomoranfernando@gmail.com>
2023-02-06 19:26:26 -0600

    Add conditional rendering

--- components/TheHeader/index.vue
+++ components/TheHeader/index.vue
@@ -6,16 +6,23 @@
       </nuxt-link>
       <ul class="flex justify-around items-center gap-x-5 w-full">
         <li>
-          <nuxt-link to="/">Home</nuxt-link>
+          <nuxt-link to="/home">Home</nuxt-link>
         </li>
         <li class="ml-auto flex items-center gap-x-8">
           <button @click="toggleDark()">
             <sun-icon class="w-8" v-if="isDark" />
             <moon-icon class="w-8" v-else />
           </button>
-          <nuxt-link to="/login">Login</nuxt-link>
-          <nuxt-link class="p-2 px-6 bg-gray-50 text-green-400 dark:text-slate-800 rounded-full font-semibold hover:bg-gray-200 dark:hover:bg-gray-300 transition-colors"
-            to="/signup">Sign Up</nuxt-link>
+          <div class=" flex items-center gap-x-3" v-if="!user">
+            <nuxt-link to="/login">Login</nuxt-link>
+            <nuxt-link
+              class="p-2 px-6 bg-gray-50 text-green-400 dark:text-slate-800 rounded-full font-semibold hover:bg-gray-200 dark:hover:bg-gray-300 transition-colors"
+              to="/signup">Sign Up</nuxt-link>
+          </div>
+          <div class="flex items-center gap-x-3" v-else>
+            <nuxt-img width="52" loading="lazy" class=" rounded-full" :src="`https://unavatar.io/${user.user_metadata?.user_name}`" />
+            <p>{{ user.user_metadata?.user_name }}</p>
+          </div>
         </li>
       </ul>
     </nav>
@@ -23,10 +30,13 @@
 </template>
 
 <script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core';
-import { MoonIcon, SunIcon } from '@heroicons/vue/24/solid';
+import { useDark, useToggle } from '@vueuse/core'
+import { MoonIcon, SunIcon } from '@heroicons/vue/24/solid'
+
+const user = useSupabaseUser()
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+
 </script>
 
 <style scoped>
